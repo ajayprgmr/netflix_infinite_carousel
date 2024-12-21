8f0e18cae77f35ea59418ce306d23f8f56e0cd9f
Ajay Kushwaha <ajay.kushwaha@horizonind.org>
2024-12-21 15:53:04 +0530

    resp, working

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 const slider = document.querySelector('.slider');
 const carousel = document.querySelector('.carousel');
 const sections = document.querySelectorAll('.slider section');
+const noOfSections = sections.length;
+const noOfSectionsPerScreen = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--no-of-sections-per-screen'));
+console.log(noOfSectionsPerScreen);
 
 let direction;
 
@@ -13,33 +16,31 @@ next.addEventListener('click', function () {
     direction = -1;
     carousel.style.justifyContent = 'flex-start';
     slider.style.transition = 'transform 0.5s ease';
-    slider.style.transform = 'translateX(-300%)';
+    slider.style.transform = `translateX(-${noOfSectionsPerScreen*100}%)`;
 });
 
 prev.addEventListener('click', function () {
     direction = 1;
     carousel.style.justifyContent = 'flex-end';
     slider.style.transition = 'transform 0.5s ease';
-    slider.style.transform = 'translateX(300%)';
+    slider.style.transform = `translateX(${noOfSectionsPerScreen*100}%)`;
 });
 
 
 
 slider.addEventListener('transitionend', function () {
     const sectionArray = Array.from(sections); // Convert NodeList to array
-    const nodesToAppend = sectionArray.slice(0, 4); // Get the first 4 sections to append
-
-    // Log the nodes to append for debugging purposes
-    console.log(nodesToAppend);
+    const nodesToAppend = sectionArray.slice(0, noOfSectionsPerScreen); // Get the first noOfSectionsPerScreen  to append
+    console.log('nodes to append',nodesToAppend);
 
     // Append the nodes to the slider based on the direction
     if (direction === -1) {
-        // Direction -1: Move the first 4 sections to the end (append them)
+        // Direction -1: Move the first noOfSectionsPerScreen sections to the end (append them)
         nodesToAppend.forEach(node => {
             slider.appendChild(node);
         });
     } else if (direction === 1) {
-        // Direction 1: Move the last 4 sections to the front (prepend them)
+        // Direction 1: Move the last noOfSectionsPerScreen sections to the front (prepend them)
         nodesToAppend.forEach(node => {
             slider.prepend(node);
         });
