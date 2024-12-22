0899230f51380d1e7f77ff542296d3f1efd0fdff
Ajay Kushwaha <ajay.kushwaha@horizonind.org>
2024-12-22 08:59:57 +0530

    right slide is working left i need to fix

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 'use strict'
-
 const slider = document.querySelector('.slider');
 const carousel = document.querySelector('.carousel');
-const sections = document.querySelectorAll('.slider section');
+let sections = document.querySelectorAll('.slider section');
 const noOfSections = sections.length;
 const noOfSectionsPerScreen = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--no-of-sections-per-screen'));
 console.log(noOfSectionsPerScreen);
@@ -12,27 +11,32 @@ let direction;
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 
+// Function to update the section reference after changing DOM
+function updateSections() {
+    sections = document.querySelectorAll('.slider section');
+    console.log('updated Sections',sections);
+}
+
 next.addEventListener('click', function () {
     direction = -1;
     carousel.style.justifyContent = 'flex-start';
     slider.style.transition = 'transform 0.5s ease';
-    slider.style.transform = `translateX(-${noOfSectionsPerScreen*100}%)`;
+    slider.style.transform = `translateX(-${100}vw)`;
 });
 
 prev.addEventListener('click', function () {
     direction = 1;
     carousel.style.justifyContent = 'flex-end';
     slider.style.transition = 'transform 0.5s ease';
-    slider.style.transform = `translateX(${noOfSectionsPerScreen*100}%)`;
+    slider.style.transform = `translateX(${100}vw)`;
 });
 
 
 
 slider.addEventListener('transitionend', function () {
+    updateSections();
     const sectionArray = Array.from(sections); // Convert NodeList to array
-    const nodesToAppend = sectionArray.slice(0, noOfSectionsPerScreen); // Get the first noOfSectionsPerScreen  to append
-    console.log('nodes to append',nodesToAppend);
-
+    const nodesToAppend = sectionArray.slice(0,noOfSectionsPerScreen); 
     // Append the nodes to the slider based on the direction
     if (direction === -1) {
         // Direction -1: Move the first noOfSectionsPerScreen sections to the end (append them)
@@ -53,6 +57,6 @@ slider.addEventListener('transitionend', function () {
     // Re-enable the transition with a small delay
     setTimeout(function () {
         slider.style.transition = 'transform 0.5s ease';
-    }, 50);
+    });
 });
 
